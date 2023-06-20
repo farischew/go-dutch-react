cd0e883b39c8f57d123a00326a435b055a9cc8d6
Faris Chew <farischew@gmail.com>
2023-06-20 17:01:23 +0800

    Adds update_items to constants URLs

--- src/constants/url.js
+++ src/constants/url.js
@@ -2,5 +2,6 @@ export const CLEAR_URL = "https://go-dutch-backend.herokuapp.com/clear";
 export const UPLOAD_URL = "https://go-dutch-backend.herokuapp.com/upload_image";
 export const ADD_URL = "https://go-dutch-backend.herokuapp.com/add_people";
 export const GET_URL = "https://go-dutch-backend.herokuapp.com/see_people";
+export const UPDATE_URL = "https://go-dutch-backend.herokuapp.com/update_items";
 export const GET_FINAL_URL =
   "https://go-dutch-backend.herokuapp.com/items_splits";
