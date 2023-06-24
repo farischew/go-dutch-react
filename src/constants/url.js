1b0594c4f254653d9370e6e4e1aa1f9d28c6eaee
Faris Chew <farischew@gmail.com>
2023-06-24 15:16:09 +0800

    Add development env

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,7 +1,14 @@
-export const CLEAR_URL = "https://go-dutch-backend.herokuapp.com/clear";
-export const UPLOAD_URL = "https://go-dutch-backend.herokuapp.com/upload_image";
-export const ADD_URL = "https://go-dutch-backend.herokuapp.com/add_people";
-export const GET_URL = "https://go-dutch-backend.herokuapp.com/see_people";
-export const UPDATE_URL = "https://go-dutch-backend.herokuapp.com/update_items";
-export const GET_FINAL_URL =
-  "https://go-dutch-backend.herokuapp.com/items_splits";
+// export const CLEAR_URL = "https://go-dutch-backend.herokuapp.com/clear";
+// export const UPLOAD_URL = "https://go-dutch-backend.herokuapp.com/upload_image";
+// export const ADD_URL = "https://go-dutch-backend.herokuapp.com/add_people";
+// export const GET_URL = "https://go-dutch-backend.herokuapp.com/see_people";
+// export const UPDATE_URL = "https://go-dutch-backend.herokuapp.com/update_items";
+// export const GET_FINAL_URL =
+//   "https://go-dutch-backend.herokuapp.com/items_splits";
+
+export const CLEAR_URL = "http://127.0.0.1:8000/clear";
+export const UPLOAD_URL = "http://127.0.0.1:8000/upload_image";
+export const ADD_URL = "http://127.0.0.1:8000/add_people";
+export const GET_URL = "http://127.0.0.1:8000/see_people";
+export const UPDATE_URL = "http://127.0.0.1:8000/update_items";
+export const GET_FINAL_URL = "http://127.0.0.1:8000/items_splits";
