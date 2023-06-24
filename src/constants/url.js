a1b3003e4181aa07f656e186ed85722c4b2569fd
Faris Chew <farischew@gmail.com>
2023-06-24 16:06:19 +0800

    Adds URLS for get_taxes and set_taxes

--- src/constants/url.js
+++ src/constants/url.js
@@ -12,3 +12,5 @@ export const ADD_URL = "http://127.0.0.1:8000/add_people";
 export const GET_URL = "http://127.0.0.1:8000/see_people";
 export const UPDATE_URL = "http://127.0.0.1:8000/update_items";
 export const GET_FINAL_URL = "http://127.0.0.1:8000/items_splits";
+export const GET_TAXES = "http://127.0.0.1:8000/get_taxes";
+export const SET_TAXES = "http://127.0.0.1:8000/set_taxes";
