c87056594a28692a15a5504dadf0d785901d6707
Faris Chew <farischew@gmail.com>
2023-06-24 18:42:20 +0800

    Adds overflow scroll to container

--- src/pages/final-dashboard.js
+++ src/pages/final-dashboard.js
@@ -31,19 +31,23 @@ export default function FinalDashboard() {
   }, [ctx.finalOutput, getFinalItemsHandler]);
 
   return (
-    <Container>
-      <h1 className="text-3xl font-bold pt-6 pb-8 text-brand-secondary">
-        Confirm your Split
-      </h1>
-      <div>
-        <FinalDashboardList finalSplit={finalOutput} />
+    <>
+      <Container>
+        <h1 className="text-3xl font-bold pt-6 pb-8 text-brand-secondary">
+          Confirm your Split
+        </h1>
+        <div>
+          <FinalDashboardList finalSplit={finalOutput} />
+        </div>
+      </Container>
+      <div className="bg-brand-background h-60">
+        <div className="flex justify-center">
+          <NewReceiptButton
+            title={"Add a New Receipt"}
+            location={"/image_upload"}
+          />
+        </div>
       </div>
-      <div className="flex justify-center">
-        <NewReceiptButton
-          title={"Add a New Receipt"}
-          location={"/image_upload"}
-        />
-      </div>
-    </Container>
+    </>
   );
 }
